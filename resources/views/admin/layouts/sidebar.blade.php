2adf6f077515e9303dfbb7647cc732d4896a0787
cevin196 <cevin196@gmail.com>
2023-03-22 23:57:53 +0700

    cleanup sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -6,18 +6,90 @@
         <img src="{{ asset('images/public/Logo.png') }}" alt="Company Logo" class="w-12 lg:w-20 mx-auto ">
 
     </a>
-    <ul class="relative px-1">
+
+
+    @php
+        $status = true;
+        $alternatives = $alternativeModel->all();
+        foreach ($alternatives as $alternative) {
+            $alternative->checkStatus() ? '' : ($status = false);
+        }
+    @endphp
+    <ul class="relative px-1 border-b">
         <li class="relative" id="sidenavSecEx1">
-            <a class="flex items-center text-sm py-4 px-6 h-12
+            <a class="flex items-center text-sm py-4 px-6 h-12 bg
             overflow-hidden {{ request()->routeIs('dashboard') ? 'text-amber-500' : 'text-gray-700' }} text-ellipsis whitespace-nowrap rounded hover:text-amber-500 transition
             duration-300 ease-in-out "
                 href="{{ route('dashboard') }}" data-mdb-ripple="true" data-mdb-ripple-color="orange">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-5 h-5 mr-2 text-primary">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>
+
                 <span class="font-bold">Dashboard</span>
             </a>
         </li>
+    </ul>
+    <ul class="relative px-1 mt-3 border-b">
+        <li class=" px-6 text-sm  text-gray-700 font-bold ">
+            Job Priority
+        </li>
+        <li class="relative">
+            <a class="flex items-center {{ $status ? '' : 'bg-gray-400 hover:text-gray-700' }} {{ request()->routeIs('jobPriority.index') ? 'text-amber-500' : 'text-gray-700' }} text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-amber-500 transition duration-300 ease-in-out "
+                href="{{ $status ? route('jobPriority.index') : '#' }}" data-mdb-ripple="true"
+                data-mdb-ripple-color="{{ $status ? 'orange' : '' }}">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                    class="bi bi-list-ol w-5 h-5 mr-2 text-green-500" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z" />
+                    <path
+                        d="M1.713 11.865v-.474H2c.217 0 .363-.137.363-.317 0-.185-.158-.31-.361-.31-.223 0-.367.152-.373.31h-.59c.016-.467.373-.787.986-.787.588-.002.954.291.957.703a.595.595 0 0 1-.492.594v.033a.615.615 0 0 1 .569.631c.003.533-.502.8-1.051.8-.656 0-1-.37-1.008-.794h.582c.008.178.186.306.422.309.254 0 .424-.145.422-.35-.002-.195-.155-.348-.414-.348h-.3zm-.004-4.699h-.604v-.035c0-.408.295-.844.958-.844.583 0 .96.326.96.756 0 .389-.257.617-.476.848l-.537.572v.03h1.054V9H1.143v-.395l.957-.99c.138-.142.293-.304.293-.508 0-.18-.147-.32-.342-.32a.33.33 0 0 0-.342.338v.041zM2.564 5h-.635V2.924h-.031l-.598.42v-.567l.629-.443h.635V5z" />
+                </svg>
+                <span>Calculation Detail</span>
+            </a>
+        </li>
+
+        <li class="relative" id="sidenavXxEx2">
+            <a href="{{ route('alternative.index') }}"
+                class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 {{ request()->routeIs(['alternative*']) ? 'text-amber-500' : 'text-gray-700' }} text-ellipsis whitespace-nowrap rounded hover:text-amber-500 transition duration-300 ease-in-out cursor-pointer"
+                data-mdb-ripple="true" data-mdb-ripple-color="orange">
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-5 h-5 mr-2 text-amber-800">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M6 6.878V6a2.25 2.25 0 012.25-2.25h7.5A2.25 2.25 0 0118 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 004.5 9v.878m13.5-3A2.25 2.25 0 0119.5 9v.878m0 0a2.246 2.246 0 00-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0121 12v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6c0-.98.626-1.813 1.5-2.122" />
+                </svg>
+
+                <span>Alternatives</span>
+            </a>
+        </li>
+        <li class="relative" id="sidenavXxEx3">
+            <a href="{{ route('criteria.index') }}"
+                class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 {{ request()->routeIs(['criteria*']) ? 'text-amber-500' : 'text-gray-700' }} text-ellipsis whitespace-nowrap rounded hover:text-amber-500 transition duration-300 ease-in-out cursor-pointer"
+                data-mdb-ripple="true" data-mdb-ripple-color="orange">
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-5 h-5 mr-2 text-amber-600">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                </svg>
+
+
+                <span>Criterias</span>
+
+            </a>
+
+        </li>
+    </ul>
+
+    <ul class="relative px-1 mt-3">
+        <li class=" px-6 text-sm  text-gray-700 font-bold ">
+            Management
+        </li>
         <li class="relative" id="sidenavSecEx2">
             <a href="{{ route('finance.index') }}"
-                class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-amber-500 transition duration-300 ease-in-out cursor-pointer"
+                class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden {{ request()->routeIs('finance.index') ? 'text-amber-500' : 'text-gray-700' }} text-ellipsis whitespace-nowrap rounded hover:text-amber-500 transition duration-300 ease-in-out cursor-pointer"
                 data-mdb-ripple="true" data-mdb-ripple-color="orange">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-5 h-5 mr-2 text-green-500">
@@ -29,7 +101,7 @@
         </li>
         <li class="relative" id="sidenavSecEx3">
             <a href="{{ route('job.index') }}"
-                class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-amber-500 transition duration-300 ease-in-out cursor-pointer"
+                class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden {{ request()->routeIs('job.index') ? 'text-amber-500' : 'text-gray-700' }} text-ellipsis whitespace-nowrap rounded hover:text-amber-500 transition duration-300 ease-in-out cursor-pointer"
                 data-mdb-ripple="true" data-mdb-ripple-color="orange">
 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -82,54 +154,6 @@
             </ul>
         </li>
 
-    </ul>
-    <hr class="my-2">
-    @php
-        $status = true;
-        $alternatives = $alternativeModel->all();
-        foreach ($alternatives as $alternative) {
-            $alternative->checkStatus() ? '' : ($status = false);
-        }
-    @endphp
-    <ul class="relative px-1">
-        <li class="relative">
-            <a class="flex items-center {{ $status ? '' : 'bg-gray-400 hover:text-gray-700' }} text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-amber-500 transition duration-300 ease-in-out "
-                href="{{ $status ? route('jobPriority.index') : '#' }}" data-mdb-ripple="true"
-                data-mdb-ripple-color="{{ $status ? 'orange' : '' }}">
-                <span class="font-bold">Job Priority</span>
-            </a>
-        </li>
-        <li class="relative" id="sidenavXxEx2">
-            <a href="{{ route('alternative.index') }}"
-                class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 {{ request()->routeIs(['alternative*']) ? 'text-amber-500' : 'text-gray-700' }} text-ellipsis whitespace-nowrap rounded hover:text-amber-500 transition duration-300 ease-in-out cursor-pointer"
-                data-mdb-ripple="true" data-mdb-ripple-color="orange">
-
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-5 h-5 mr-2 text-amber-800">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M6 6.878V6a2.25 2.25 0 012.25-2.25h7.5A2.25 2.25 0 0118 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 004.5 9v.878m13.5-3A2.25 2.25 0 0119.5 9v.878m0 0a2.246 2.246 0 00-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0121 12v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6c0-.98.626-1.813 1.5-2.122" />
-                </svg>
-
-                <span>Alternatives</span>
-            </a>
-        </li>
-        <li class="relative" id="sidenavXxEx3">
-            <a href="{{ route('criteria.index') }}"
-                class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 {{ request()->routeIs(['criteria*']) ? 'text-amber-500' : 'text-gray-700' }} text-ellipsis whitespace-nowrap rounded hover:text-amber-500 transition duration-300 ease-in-out cursor-pointer"
-                data-mdb-ripple="true" data-mdb-ripple-color="orange">
-
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-5 h-5 mr-2 text-amber-600">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
-                </svg>
-
-
-                <span>Criterias</span>
-
-            </a>
-
-        </li>
     </ul>
     <div class="text-center bottom-0 absolute w-full bg-white">
         <hr class="m-0">
